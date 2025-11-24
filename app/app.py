0f3370e82b70bbcfa3627114c95c0a0b73665b6e
Amira Bekhta <amira25bkh@gmail.com>
2025-11-24 17:40:21 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import joblib
 import os
 
-model = joblib.load("model.pkl")
+model = joblib.load(os.path.join(os.path.dirname(__file__), "..", "model/model.pkl"))
 
 app = Flask(__name__)
 
